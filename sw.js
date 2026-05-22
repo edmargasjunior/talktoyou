3ed087c3b7eb87298efab1b043d7ffa7963d9840
edmargasjunior <edmargasjunior@gmail.com>
2026-05-21 21:49:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -25,7 +25,8 @@ IndexedDB/localStorage do aparelho.
 
     Isso força o navegador/app a criar um novo cache e remover o antigo.
 */
-const CACHE_NAME = "talktoyou-cache-v1";
+const CACHE_VERSION = "1";
+const CACHE_NAME = `talktoyou-cache-v${CACHE_VERSION}`;
 
 /*
     Lista de arquivos essenciais para funcionamento básico offline.
